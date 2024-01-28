ec59585e5b0c726b6f0033420cdefaaee704642c
Salted Cracker Jack <87721660+oneSALTycracker@users.noreply.github.com>
2024-01-28 11:27:29 -0500

    Update modloop.py

--- modloop.py
+++ modloop.py
@@ -3,24 +3,27 @@ from pavlov import PavlovRCON
 import config  
 
 async def remove_mods():
-    while True:
-        try:
-            
-            pavlov = PavlovRCON(config.IP_ADDRESS, config.PORT, config.PASSWORD)
+    pavlov = PavlovRCON(config.IP_ADDRESS, config.PORT, config.PASSWORD)
+    try:
+        while True:
+            try:
+                response = await pavlov.send("UGCClearModList")
+                print(response)
 
-            
-            response = await pavlov.send("UGCClearModList")
-            print(response)
+                # Check if the command was successful
+                if response.get("Successful"):
+                    print("Command executed successfully.")
+                else:
+                    print("Command failed.")
 
-
-            
-            await pavlov.send("Disconnect")
-            pavlov.close()
-
-        except Exception as e:
-            print(f"An error occurred: {e}")
-
-        
-        await asyncio.sleep(config.DELAY)  # Waits for 60 seconds before the next loop
+                await asyncio.sleep(config.DELAY)
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                await asyncio.sleep(config.DELAY)
+    except KeyboardInterrupt:
+        print("Exiting script.")
+    finally:
+        await pavlov.send("Disconnect")
+        pavlov.close()
 
 asyncio.run(remove_mods())
